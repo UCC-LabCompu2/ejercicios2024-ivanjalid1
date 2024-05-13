f8b816ffd8655d46c125b3ffcc1a500ffd871aee
Ivan <163861405+ivanjalid1@users.noreply.github.com>
2024-05-13 10:39:05 -0300

    se agrega ej

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,39 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirUnidades(nombre, valor){
+    if(isNaN(valor)){
+        alert("El valor ingresado no es un número");
+
+        document.getElementById("metro").value = "";
+        document.getElementById("pulgada").value = "";
+        document.getElementById("pie").value = "";
+        document.getElementById("yarda").value = "";
+
+    }   else if(nombre === "metro"){
+
+        document.getElementById("pulgada").value = valor*39.7;
+        document.getElementById("pie").value = valor*3.24;
+        document.getElementById("yarda").value = valor*1.093;
+
+    }   else if(nombre === "pulgada"){
+
+        document.getElementById("metro").value = valor/39.7; // hacer todas las conversiones correctas
+        document.getElementById("pie").value = valor*3.24;
+        document.getElementById("yarda").value = valor*1.093;
+
+    }   else if(nombre === "pie"){
+        
+        document.getElementById("pulgada").value = valor*39.7;
+        document.getElementById("pie").value = valor*3.24;
+        document.getElementById("yarda").value = valor*1.093;
+
+    }   else if(nombre === "yarda"){
+
+        document.getElementById("pulgada").value = valor*39.7;
+        document.getElementById("pie").value = valor*3.24;
+        document.getElementById("yarda").value = valor*1.093;
+    }
+
+}
